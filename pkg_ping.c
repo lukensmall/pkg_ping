681274a239cf3223ff73d3d170fd35420be90415
Luke N Small <lukensmall@gmail.com>
2018-10-19 13:23:02 -0500

    Update pkg_ping.c

--- pkg_ping.c
+++ pkg_ping.c
@@ -1,7 +1,7 @@
 /*
  * BSD 2-Clause License
  *
- * Copyright (c) 2017, Luke N Small, lukensmall@gmail.com
+ * Copyright (c) 2017, 2018, Luke N Small, lukensmall@gmail.com
  * All rights reserved.
  
  * Redistribution and use in source and binary forms, with or without
