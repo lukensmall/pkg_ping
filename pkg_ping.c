053b20a78fd596669f55842765053edf1ed6b91d
Luke N Small <lukensmall@gmail.com>
2019-08-16 22:49:22 -0500

    Update pkg_ping.c

--- pkg_ping.c
+++ pkg_ping.c
@@ -3,6 +3,7 @@
  *
  * Copyright (c) 2017, 2018, 2019, Luke N Small, lukensmall@gmail.com
  * All rights reserved.
+ * 
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
