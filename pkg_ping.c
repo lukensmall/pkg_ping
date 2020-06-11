3b6e5a77313baa91d93a14973640f9d68fb7a4fe
Luke N Small <lukensmall@gmail.com>
2020-06-10 23:25:31 -0500

    Update pkg_ping.c

--- pkg_ping.c
+++ pkg_ping.c
@@ -309,7 +309,7 @@ main(int argc, char *argv[])
 		if (verbose < 1)
 			verbose = 1;
 		secure = 1;
-		dns_cache_d = 1;
+		dns_cache_d = 0;
 		f = 0;
 		if (pledge("stdio exec proc dns id", NULL) == -1)
 			err(1, "pledge, line: %d", __LINE__);
@@ -673,34 +673,32 @@ jump_f:
 	entry_line = __LINE__;
 
 
-	char *ftp_list[58] = {
+	char *ftp_list[52] = {
 
 		"ftp.bit.nl","ftp.fau.de","ftp.fsn.hu","openbsd.hk",
 		"ftp.eenet.ee","ftp.nluug.nl","ftp.riken.jp","ftp.cc.uoc.gr",
-		"ftp.heanet.ie","ftp.spline.de","www.ftp.ne.jp",
-		"ftp.icm.edu.pl","ftp.yzu.edu.tw","mirror.one.com",
-		"mirrors.nav.ro","cdn.openbsd.org","ftp.OpenBSD.org",
-		"mirror.esc7.net","mirror.vdms.com","mirrors.mit.edu",
-		"ftp.bytemine.net","mirror.labkom.id","mirror.litnet.lt",
-		"mirror.yandex.ru","ftp.hostserver.de","mirrors.sonic.net",
-		"mirrors.ucr.ac.cr","ftp.eu.openbsd.org","ftp.fr.openbsd.org",
-		"mirror.fsmg.org.nz","mirror.linux.pizza","mirror.ungleich.ch",
-		"mirrors.dotsrc.org","openbsd.ipacct.com","ftp.usa.openbsd.org",
-		"ftp2.eu.openbsd.org","mirror.leaseweb.com",
-		"mirrors.dalenys.com","mirrors.gigenet.com",
-		"ftp4.usa.openbsd.org","mirror.aarnet.edu.au",
+		"ftp.heanet.ie","www.ftp.ne.jp","ftp.icm.edu.pl",
+		"ftp.yzu.edu.tw","mirror.one.com","cdn.openbsd.org",
+		"ftp.OpenBSD.org","mirror.esc7.net","mirror.vdms.com",
+		"mirrors.mit.edu","mirror.litnet.lt","mirror.yandex.ru",
+		"ftp.hostserver.de","mirrors.sonic.net","mirrors.ucr.ac.cr",
+		"ftp.eu.openbsd.org","ftp.fr.openbsd.org","mirror.linux.pizza",
+		"mirror.ungleich.ch","mirrors.dotsrc.org","openbsd.ipacct.com",
+		"ftp.usa.openbsd.org","ftp2.eu.openbsd.org",
+		"mirror.leaseweb.com","mirrors.dalenys.com",
+		"mirrors.gigenet.com","ftp4.usa.openbsd.org",
+		"mirror.aarnet.edu.au","mirror.exonetric.net",
 		"openbsd.c3sl.ufpr.br","*artfiles.org/openbsd",
-		"mirror.bytemark.co.uk","mirror.planetunix.net",
-		"www.mirrorservice.org","mirror.hs-esslingen.de",
-		"mirrors.pidginhost.com","openbsd.cs.toronto.edu",
-		"openbsd.mirror.garr.it","cloudflare.cdn.openbsd.org",
-		"ftp.halifax.rwth-aachen.de","ftp.rnl.tecnico.ulisboa.pt",
-		"mirror.csclub.uwaterloo.ca","mirrors.syringanetworks.net",
+		"mirror.planetunix.net","www.mirrorservice.org",
+		"mirror.hs-esslingen.de","mirrors.pidginhost.com",
+		"openbsd.cs.toronto.edu","openbsd.mirror.garr.it",
+		"cloudflare.cdn.openbsd.org","ftp.halifax.rwth-aachen.de",
+		"ftp.rnl.tecnico.ulisboa.pt","mirror.csclub.uwaterloo.ca",
 		"openbsd.mirror.constant.com","plug-mirror.rcac.purdue.edu",
 		"openbsd.mirror.netelligent.ca"
 	};
 
-	int index = arc4random_uniform(58);
+	int index = arc4random_uniform(52);
 
 
 	exit_line = __LINE__;
